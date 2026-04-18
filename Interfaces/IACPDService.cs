54d460a1509142c72281e9e413d58ca50dbbd5bc
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 10:50:48 +0800

    feat: ACPD create finish.

--- Interfaces/IACPDService.cs
+++ Interfaces/IACPDService.cs
@@ -1,8 +1,10 @@
-﻿namespace MercuryTest.Interfaces
+﻿using MercuryTest.Models;
+
+namespace MercuryTest.Interfaces
 {
     public interface IACPDService
     {
-        //Task<bool> Create(MyOffice_ACPD acpd);
+        Task<bool> Create(MyOffice_ACPD acpd);
         Task<string> READ();
         //Task<bool> Update(MyOffice_ACPD acpd);
         //Task<bool> Delete(string sid);
--- Services/ACPDService.cs
+++ Services/ACPDService.cs
@@ -1,6 +1,9 @@
-﻿using System.Text.Json;
+﻿using System.Data;
+using System.Text.Json;
+using Dapper;
 using MercuryTest.Data;
 using MercuryTest.Interfaces;
+using MercuryTest.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MercuryTest.Services
@@ -8,10 +11,12 @@ namespace MercuryTest.Services
     public class ACPDService : IACPDService
     {
         private readonly AppDbContext _appDbContext;
+        private readonly IDbConnection _dbContext;
 
-        public ACPDService(AppDbContext appDbContext)
+        public ACPDService(AppDbContext appDbContext, IDbConnection dbContext)
         {
             _appDbContext = appDbContext;
+            _dbContext = dbContext;
         }
 
         public async Task<string> READ()
@@ -19,6 +24,38 @@ namespace MercuryTest.Services
             var data = await _appDbContext.MyOffice_ACPD.ToListAsync();
             string json = JsonSerializer.Serialize(data);
             return json;
+        }
+
+        public async Task<bool> Create(MyOffice_ACPD acpd)
+        {
+            var param = new DynamicParameters();
+            var parameters = new DynamicParameters();
+            parameters.Add("@TableName", "MyOffice_ACPD", DbType.String, ParameterDirection.Input);
+
+            parameters.Add(
+                "@ReturnSID",
+                dbType: DbType.String,
+                direction: ParameterDirection.Output,
+                size: 20
+            );
+
+            await _dbContext.ExecuteAsync(
+                "NEWSID",
+                parameters,
+                commandType: CommandType.StoredProcedure
+            );
+
+            string newSid = parameters.Get<string>("@ReturnSID");
+            DateTime now = DateTime.Now;
+
+            if (string.IsNullOrEmpty(newSid))
+                return false;
+
+            acpd.ACPD_SID = newSid;
+            acpd.ACPD_NowDateTime = now;
+            acpd.ACPD_UPDDateTime = now;
+            await _appDbContext.MyOffice_ACPD.AddAsync(acpd);
+            return await _appDbContext.SaveChangesAsync() > 0;
         }
     }
 }
