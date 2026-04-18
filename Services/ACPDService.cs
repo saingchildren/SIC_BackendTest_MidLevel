d47f829c84ebd4be273178b9c477671efeaf66ec
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 10:35:10 +0800

    feat: ACPD Read finish.

---
+++ Services/ACPDService.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using MercuryTest.Data;
+using MercuryTest.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MercuryTest.Services
+{
+    public class ACPDService : IACPDService
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ACPDService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<string> READ()
+        {
+            var data = await _appDbContext.MyOffice_ACPD.ToListAsync();
+            string json = JsonSerializer.Serialize(data);
+            return json;
+        }
+    }
+}
