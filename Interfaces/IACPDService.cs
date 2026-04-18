55213ef204c3ff7ba215c296c92871bdd990e3da
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 10:34:39 +0800

    feat: define ACPDService interface & READ first implement

---
+++ Interfaces/IACPDService.cs
@@ -0,0 +1,10 @@
+﻿namespace MercuryTest.Interfaces
+{
+    public interface IACPDService
+    {
+        //Task<bool> Create(MyOffice_ACPD acpd);
+        Task<string> READ();
+        //Task<bool> Update(MyOffice_ACPD acpd);
+        //Task<bool> Delete(string sid);
+    }
+}
