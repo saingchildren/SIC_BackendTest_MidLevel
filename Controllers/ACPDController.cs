3c0538ba37fc5b0f71c198b83c0752aad6b55f10
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 11:39:36 +0800

    feat: fix ACPDController route.

--- Controllers/ACPDController.cs
+++ Controllers/ACPDController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace MercuryTest.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/myofficeacpd/[controller]")]
     public class ACPDController : Controller
     {
         private readonly IACPDService _aCPDService;
