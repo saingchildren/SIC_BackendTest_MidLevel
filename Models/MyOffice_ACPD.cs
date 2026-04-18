83374ea90235dd7b623f778c0f076949bc3f53f6
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 10:29:13 +0800

    feat: create MyOfficef_ACPD C# model

---
+++ Models/MyOffice_ACPD.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MercuryTest.Models
+{
+    public class MyOffice_ACPD
+    {
+        [StringLength(20, ErrorMessage = "SID 長度不超過20字元")]
+        public string ACPD_SID { get; set; } = string.Empty;
+
+        [StringLength(60, ErrorMessage = "中文名稱不超過60字元")]
+        public string? ACPD_Cname { get; set; }
+
+        [StringLength(40)]
+        public string? ACPD_Ename { get; set; }
+
+        [StringLength(40)]
+        public string? ACPD_Sname { get; set; }
+
+        [StringLength(60)]
+        [EmailAddress(ErrorMessage = "Email 格式不正確")]
+        public string? ACPD_Email { get; set; }
+        public byte? ACPD_Status { get; set; }
+        public bool? ACPD_Stop { get; set; }
+
+        [StringLength(60)]
+        public string? ACPD_StopMemo { get; set; }
+
+        [StringLength(30)]
+        public string? ACPD_LoginID { get; set; }
+
+        [StringLength(60)]
+        public string? ACPD_LoginPWD { get; set; }
+
+        [StringLength(600)]
+        public string? ACPD_Memo { get; set; }
+        public DateTime? ACPD_NowDateTime { get; set; }
+
+        [StringLength(20)]
+        public string? ACPD_NowID { get; set; }
+        public DateTime? ACPD_UPDDateTime { get; set; }
+
+        [StringLength(20)]
+        public string? ACPD_UPDID { get; set; }
+    }
+}
