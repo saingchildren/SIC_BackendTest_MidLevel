74b0d134abd0a378fe6c4d4639d3310a3dce361c
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 11:23:18 +0800

    feat: add log model to AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,12 +9,14 @@ namespace MercuryTest.Data
             : base(options) { }
 
         public DbSet<MyOffice_ACPD> MyOffice_ACPD { get; set; }
+        public DbSet<MyOffice_ExcuteionLog> MyOffice_ExcuteionLog { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<MyOffice_ACPD>().HasKey(m => m.ACPD_SID);
+            modelBuilder.Entity<MyOffice_ExcuteionLog>().HasKey(m => m.DeLog_AutoID);
         }
     }
 }
