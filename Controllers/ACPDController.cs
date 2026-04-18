eda9ae47a9804771de3a8e018442081178aacdd3
saingchildren <80457007+saingchildren@users.noreply.github.com>
2026-04-18 10:35:26 +0800

    feat: ACPDController Get finish

---
+++ Controllers/ACPDController.cs
@@ -0,0 +1,46 @@
+﻿using MercuryTest.Interfaces;
+using MercuryTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercuryTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ACPDController : Controller
+    {
+        private readonly IACPDService _aCPDService;
+
+        public ACPDController(IACPDService aCPDService)
+        {
+            _aCPDService = aCPDService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await _aCPDService.READ();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] MyOffice_ACPD acpd)
+        {
+            try
+            {
+                var result = await _aCPDService.Create(acpd);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
